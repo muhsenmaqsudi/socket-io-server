2c404f78350cb8bc6d5dbd67c21111fa08c56121
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-08-31 17:31:30 +0430

    added user is typeing sample

--- src/socket-io/socket-io.gateway.ts
+++ src/socket-io/socket-io.gateway.ts
@@ -13,12 +13,11 @@ import { Server, Client, Socket } from 'socket.io';
 @WebSocketGateway()
 export class SocketIoGateway
   implements OnGatewayConnection, OnGatewayDisconnect {
-  @WebSocketServer()
-  wss: Server;
+  @WebSocketServer() private wss: Server;
 
   handleConnection(client: Client, ...args: any[]) {
     console.log(`Client with id ${client.id} has been connected to our wss`);
-    this.wss.emit('customEmit', 'data passed from wss');
+    this.wss.emit('connectedToWss', 'data passed to client from wss');
   }
 
   handleDisconnect(client: Client) {
@@ -37,5 +36,21 @@ export class SocketIoGateway
       `client's message with ${client.id} client id has been recieved`,
     );
     return { event: 'message', data };
+  }
+
+  @SubscribeMessage('userIsTypingToWss')
+  onUserIsTypingEventHandler(
+    @MessageBody() data: unknown,
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.emit('userIsTypeingToClient', 'user is typing');
+  }
+
+  @SubscribeMessage('userIsNoLongerTyping')
+  onUserIsNoLongerTypingEventHandler(
+    @MessageBody() data: unknown,
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.emit('userIsNoLongerTypingToClient', 'user is no longer typing');
   }
 }
